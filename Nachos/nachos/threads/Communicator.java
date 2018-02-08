57e813d13908196248e20835a1d3e41b8b59cb7c
Havorax <havorax@gmail.com>
2018-02-07 17:51:13 -0800

    tasks 1-3 of Proj 1
    task 4 doesn't seem to need implementing (?)

--- Nachos/nachos/threads/Communicator.java
+++ Nachos/nachos/threads/Communicator.java
@@ -10,32 +10,62 @@ import nachos.machine.*;
  * threads can be paired off at this point.
  */
 public class Communicator {
-    /**
-     * Allocate a new communicator.
-     */
-    public Communicator() {
-    }
+	/**
+	 * Allocate a new communicator.
+	 */
+	public Communicator() {
+	}
 
-    /**
-     * Wait for a thread to listen through this communicator, and then transfer
-     * <i>word</i> to the listener.
-     *
-     * <p>
-     * Does not return until this thread is paired up with a listening thread.
-     * Exactly one listener should receive <i>word</i>.
-     *
-     * @param	word	the integer to transfer.
-     */
-    public void speak(int word) {
-    }
+	/**
+	 * Wait for a thread to listen through this communicator, and then transfer
+	 * <i>word</i> to the listener.
+	 *
+	 * <p>
+	 * Does not return until this thread is paired up with a listening thread.
+	 * Exactly one listener should receive <i>word</i>.
+	 *
+	 * @param	word	the integer to transfer.
+	 */
+	public void speak(int word) {
+		waiting.acquire(); // acquire lock
+		if (!burner)	// burner is false if communicator hasn't been used before
+			while (spoke)	// spoke is true if another thread previously called speak(int)
+			{
+				waitQueue.sleep(); // so sleep until that thread is done
+			}
+		else
+			burner = false;
+		toTransfer = word;	// set message to pass
+		spoke = true;	// flag other speakers to sleep
+		waitQueue.wake();	// hopefully wake a listener
+		waiting.release();	// release lock 
+		// NOTE: order of messages conveyed is NOT deterministic and actually unlikely to occur sequentially
+	}
 
-    /**
-     * Wait for a thread to speak through this communicator, and then return
-     * the <i>word</i> that thread passed to <tt>speak()</tt>.
-     *
-     * @return	the integer transferred.
-     */    
-    public int listen() {
-	return 0;
-    }
+	/**
+	 * Wait for a thread to speak through this communicator, and then return
+	 * the <i>word</i> that thread passed to <tt>speak()</tt>.
+	 *
+	 * @return	the integer transferred.
+	 */    
+	public int listen() {
+		waiting.acquire();
+		if (!burner)
+			while (!spoke)
+			{
+				waitQueue.sleep();
+			}
+		else
+			burner = false;
+		int transferring = toTransfer;
+		spoke = false;
+		waitQueue.wake();
+		waiting.release();
+		return transferring;
+	}
+
+	private Lock waiting;
+	private Condition2 waitQueue = new Condition2(waiting);
+	private int toTransfer = 0;
+	private boolean spoke, burner = true;
 }
--- Nachos/nachos/threads/Condition2.java
+++ Nachos/nachos/threads/Condition2.java
@@ -2,6 +2,8 @@ package nachos.threads;
 
 import nachos.machine.*;
 
+import java.util.LinkedList;
+
 /**
  * An implementation of condition variables that disables interrupt()s for
  * synchronization.
@@ -12,47 +14,63 @@ import nachos.machine.*;
  * @see	nachos.threads.Condition
  */
 public class Condition2 {
-    /**
-     * Allocate a new condition variable.
-     *
-     * @param	conditionLock	the lock associated with this condition
-     *				variable. The current thread must hold this
-     *				lock whenever it uses <tt>sleep()</tt>,
-     *				<tt>wake()</tt>, or <tt>wakeAll()</tt>.
-     */
-    public Condition2(Lock conditionLock) {
-	this.conditionLock = conditionLock;
-    }
+	/**
+	 * Allocate a new condition variable.
+	 *
+	 * @param	conditionLock	the lock associated with this condition
+	 *				variable. The current thread must hold this
+	 *				lock whenever it uses <tt>sleep()</tt>,
+	 *				<tt>wake()</tt>, or <tt>wakeAll()</tt>.
+	 */
+	public Condition2(Lock conditionLock) {
+		this.conditionLock = conditionLock;
 
-    /**
-     * Atomically release the associated lock and go to sleep on this condition
-     * variable until another thread wakes it using <tt>wake()</tt>. The
-     * current thread must hold the associated lock. The thread will
-     * automatically reacquire the lock before <tt>sleep()</tt> returns.
-     */
-    public void sleep() {
-	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+		waitQueue = new LinkedList<Lock>();
+	}
 
-	conditionLock.release();
+	/**
+	 * Atomically release the associated lock and go to sleep on this condition
+	 * variable until another thread wakes it using <tt>wake()</tt>. The
+	 * current thread must hold the associated lock. The thread will
+	 * automatically reacquire the lock before <tt>sleep()</tt> returns.
+	 */
+	public void sleep() {
+		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
 
-	conditionLock.acquire();
-    }
+		Lock waiter = new Lock();
+		waitQueue.add(waiter);
 
-    /**
-     * Wake up at most one thread sleeping on this condition variable. The
-     * current thread must hold the associated lock.
-     */
-    public void wake() {
-	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
-    }
+		conditionLock.release();
+		waiter.acquire();
+		conditionLock.acquire();
+	}
 
-    /**
-     * Wake up all threads sleeping on this condition variable. The current
-     * thread must hold the associated lock.
-     */
-    public void wakeAll() {
-	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
-    }
+	/**
+	 * Wake up at most one thread sleeping on this condition variable. The
+	 * current thread must hold the associated lock.
+	 */
+	public void wake() {
+		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+		
+		if (!waitQueue.isEmpty())
+		{
+			((Lock)waitQueue.removeFirst()).release();
+		}
+	}
 
-    private Lock conditionLock;
+	/**
+	 * Wake up all threads sleeping on this condition variable. The current
+	 * thread must hold the associated lock.
+	 */
+	public void wakeAll() {
+		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+		
+		while (!waitQueue.isEmpty())
+		{
+			wake();
+		}
+	}
+
+	private Lock conditionLock;
+	private LinkedList<Lock> waitQueue;
 }
