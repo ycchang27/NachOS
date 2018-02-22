2ca009536676e7f7b14f9ddb22337918d04e41ac
lex5852 <35547116+lex5852@users.noreply.github.com>
2018-02-21 18:17:46 -0800

    accidently deleted a line

--- Nachos/nachos/threads/ThreadedKernel.java
+++ Nachos/nachos/threads/ThreadedKernel.java
@@ -47,6 +47,7 @@ public class ThreadedKernel extends Kernel {
      */	
     public void selfTest() {
 	KThread.selfTest();
+	Semaphore.selfTest();
 	//Testing for condition2
 	Condition2.selfTest();
 	SynchList.selfTest();
