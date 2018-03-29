e14593326af03b1cfa8bbb8d9dd134b60b8efa06
Yun Chul Chang <ychang29@ucmerced.edu>
2018-03-29 14:26:20 -0700

    Commented out selfTest() in UserKernel
    the echo part is unnecessary

--- Nachos/nachos/userprog/UserKernel.java
+++ Nachos/nachos/userprog/UserKernel.java
@@ -47,17 +47,17 @@ public class UserKernel extends ThreadedKernel {
 	public void selfTest() {
 		super.selfTest();
 
-		System.out.println("Testing the console device. Typed characters");
-		System.out.println("will be echoed until q is typed.");
-
-		char c;
-
-		do {
-			c = (char) console.readByte(true);
-			console.writeByte(c);
-		} while (c != 'q');
-
-		System.out.println("");
+//		System.out.println("Testing the console device. Typed characters");
+//		System.out.println("will be echoed until q is typed.");
+//
+//		char c;
+//
+//		do {
+//			c = (char) console.readByte(true);
+//			console.writeByte(c);
+//		} while (c != 'q');
+//
+//		System.out.println("");
 	}
 
 	/**
