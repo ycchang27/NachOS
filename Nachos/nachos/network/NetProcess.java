dfef1042fa050355bafc732471b3525d14dd1588
Yun Chul Chang <ychang29@ucmerced.edu>
2018-04-28 15:06:14 -0700

    2/7 Test "Passing"
    "Passing" because a "Socket" class wasn't inserted into the list of filehandlers

--- Nachos/nachos/network/NetProcess.java
+++ Nachos/nachos/network/NetProcess.java
@@ -9,38 +9,85 @@ import nachos.vm.*;
  * A <tt>VMProcess</tt> that supports networking syscalls.
  */
 public class NetProcess extends UserProcess {
-    /**
-     * Allocate a new process.
-     */
-    public NetProcess() {
-	super();
-    }
+	/**
+	 * Allocate a new process.
+	 */
+	public NetProcess() {
+		super();
+		netManager = new NetCommandCenter();
+	}
 
-    private static final int
+	/**
+	 * Attempt to initiate a new connection to the specified port on the specified
+	 * remote host, and return a new file descriptor referring to the connection.
+	 * connect() does not give up if the remote host does not respond immediately.
+	 * 
+	 * @param host: remote machine address
+	 * @param port: remote port number
+	 * @return
+	 */
+	private int handleConnect(int host, int port) {
+		return netManager.connect(host, port);
+	}
+
+	/**
+	 * Attempt to accept a single connection on the specified local port and return
+	 * a file descriptor referring to the connection.
+	 *
+	 * If any connection requests are pending on the port, one request is dequeued
+	 * and an acknowledgement is sent to the remote host (so that its connect()
+	 * call can return). Since the remote host will never cancel a connection
+	 * request, there is no need for accept() to wait for the remote host to
+	 * confirm the connection (i.e. a 2-way handshake is sufficient; TCP's 3-way
+	 * handshake is unnecessary).
+	 *
+	 * If no connection requests are pending, returns -1 immediately.
+	 *
+	 * In either case, accept() returns without waiting for a remote host.
+	 *
+	 * Returns a new file descriptor referring to the connection, or -1 if an error
+	 * occurred.
+	 * 
+	 * @param port: local port on this machine
+	 * @return a new file descriptor referring to the connection, or -1 if an error
+	 * occurred.
+	 */
+	private int handleAccept(int port) {
+		return netManager.accept(port);
+	}
+
+	private static final int
 	syscallConnect = 11,
 	syscallAccept = 12;
-    
-    /**
-     * Handle a syscall exception. Called by <tt>handleException()</tt>. The
-     * <i>syscall</i> argument identifies which syscall the user executed:
-     *
-     * <table>
-     * <tr><td>syscall#</td><td>syscall prototype</td></tr>
-     * <tr><td>11</td><td><tt>int  connect(int host, int port);</tt></td></tr>
-     * <tr><td>12</td><td><tt>int  accept(int port);</tt></td></tr>
-     * </table>
-     * 
-     * @param	syscall	the syscall number.
-     * @param	a0	the first syscall argument.
-     * @param	a1	the second syscall argument.
-     * @param	a2	the third syscall argument.
-     * @param	a3	the fourth syscall argument.
-     * @return	the value to be returned to the user.
-     */
-    public int handleSyscall(int syscall, int a0, int a1, int a2, int a3) {
-	switch (syscall) {
-	default:
-	    return super.handleSyscall(syscall, a0, a1, a2, a3);
+
+	private NetCommandCenter netManager;
+
+	/**
+	 * Handle a syscall exception. Called by <tt>handleException()</tt>. The
+	 * <i>syscall</i> argument identifies which syscall the user executed:
+	 *
+	 * <table>
+	 * <tr><td>syscall#</td><td>syscall prototype</td></tr>
+	 * <tr><td>11</td><td><tt>int  connect(int host, int port);</tt></td></tr>
+	 * <tr><td>12</td><td><tt>int  accept(int port);</tt></td></tr>
+	 * </table>
+	 * 
+	 * @param	syscall	the syscall number.
+	 * @param	a0	the first syscall argument.
+	 * @param	a1	the second syscall argument.
+	 * @param	a2	the third syscall argument.
+	 * @param	a3	the fourth syscall argument.
+	 * @return	the value to be returned to the user.
+	 */
+	public int handleSyscall(int syscall, int a0, int a1, int a2, int a3) {
+		switch (syscall) {
+		case syscallConnect:
+			return handleConnect(a0, a1);
+		case syscallAccept:
+			return handleAccept(a0);
+		default:
+			Machine.halt();
+			return super.handleSyscall(syscall, a0, a1, a2, a3);
+		}
 	}
-    }
 }
